46f44fc14feb79b7a371e5139ed694f452b0db86
Jacob Short <46907893+jrshort89@users.noreply.github.com>
2021-11-27 16:03:30 -0500

    Added Climbing Animation and Ladders

--- Tilevania4/Assets/Scripts/PlayerInput.cs
+++ Tilevania4/Assets/Scripts/PlayerInput.cs
@@ -6,40 +6,39 @@ using UnityEngine.InputSystem;
 
 public class PlayerInput : MonoBehaviour
 {
-    [SerializeField] float runSpeed= 5f;
+    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 5f;
+    [SerializeField] float defaultGravity = 8f;
 
 
     Vector2 moveInput;
     Rigidbody2D myRigidBody;
     Animator myAnimator;
-    bool playerHasHorizontalSpeed;
+    int ground;
+    int climbableObject;
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        ground = LayerMask.GetMask("Ground");
+        climbableObject = LayerMask.GetMask("Climb");
     }
 
     // Update is called once per frame
     void Update()
-    {
-        playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
-        Run();        
+    {        
+        Run();
+        ChangeSpriteDirection();
+        StartClimbing();
     }
 
     void Run()
     {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
         Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidBody.velocity.y);
-        myRigidBody.velocity = playerVelocity;
-        ChangeSpriteDirection();
-        if (playerHasHorizontalSpeed)
-        {
-            myAnimator.SetBool("isRunning", true);
-        }
-        else
-        {
-            myAnimator.SetBool("isRunning", false);
-        }
-        
+        myRigidBody.velocity = playerVelocity;        
+        myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);        
     }
 
     void OnMove(InputValue value)
@@ -48,10 +47,42 @@ public class PlayerInput : MonoBehaviour
     }
 
     void ChangeSpriteDirection()
-    {        
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+
         if (playerHasHorizontalSpeed)
         {
             transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
         }         
+    }
+
+    void OnJump(InputValue value)
+    {        
+        if (myRigidBody.IsTouchingLayers(ground))
+        {            
+            myRigidBody.velocity += new Vector2(0f, jumpSpeed);
+        }        
+    }
+
+    void StartClimbing()
+    {
+        bool playerHasVerticalalSpeed = Mathf.Abs(myRigidBody.velocity.y) > Mathf.Epsilon;
+        bool isTouchingClimableObject = myRigidBody.IsTouchingLayers(climbableObject);
+        myAnimator.SetBool("isClimbing", isTouchingClimableObject);
+        if (!isTouchingClimableObject) 
+        {
+            myRigidBody.gravityScale = defaultGravity;
+            return; 
+        }                    
+        if (isTouchingClimableObject)
+        {
+            Vector2 climbVelocity = new Vector2(myRigidBody.velocity.x, moveInput.y * climbSpeed);
+            myRigidBody.velocity = climbVelocity;
+            myRigidBody.gravityScale = 0;
+        }        
+        if (!playerHasVerticalalSpeed)
+        {
+            myAnimator.SetBool("isClimbing", playerHasVerticalalSpeed);
+        }
     }
 }
