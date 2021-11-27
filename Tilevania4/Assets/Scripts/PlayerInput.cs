f0f5833a90d0f0b0293a4a33322597f6b6af48a4
Jacob Short <46907893+jrshort89@users.noreply.github.com>
2021-11-27 12:15:58 -0500

    apply running animation to player

--- Tilevania4/Assets/Scripts/PlayerInput.cs
+++ Tilevania4/Assets/Scripts/PlayerInput.cs
@@ -6,28 +6,52 @@ using UnityEngine.InputSystem;
 
 public class PlayerInput : MonoBehaviour
 {
+    [SerializeField] float runSpeed= 5f;
+
+
     Vector2 moveInput;
     Rigidbody2D myRigidBody;
-    // Start is called before the first frame update
+    Animator myAnimator;
+    bool playerHasHorizontalSpeed;
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Run();
+        playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        Run();        
     }
 
     void Run()
     {
-        myRigidBody.velocity = moveInput;
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+        ChangeSpriteDirection();
+        if (playerHasHorizontalSpeed)
+        {
+            myAnimator.SetBool("isRunning", true);
+        }
+        else
+        {
+            myAnimator.SetBool("isRunning", false);
+        }
+        
     }
 
     void OnMove(InputValue value)
     {
-        moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
+        moveInput = value.Get<Vector2>();        
+    }
+
+    void ChangeSpriteDirection()
+    {        
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }         
     }
 }
